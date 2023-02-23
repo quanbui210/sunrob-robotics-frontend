dee3bce288ea607dd52b3d4add54455ee574f21e
Quan Bui <quanbui021001@gmail.com>
2023-02-23 21:34:44 +0200

    remove trash file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import './index.css';
 import App from './App';
-import ScrollToTop from './helper/ScrollToTop';
 import cartSlice from './store/cart-slice';
 import { configureStore } from '@reduxjs/toolkit';
 
